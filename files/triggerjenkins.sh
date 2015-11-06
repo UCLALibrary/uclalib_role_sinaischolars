d22b97d26ae364b4754e44337b07350b8a8414ed
Kevin S. Clarke <ksclarke@gmail.com>
2015-11-06 18:12:48 -0500

    Update roles names to match our new convention

---
+++ files/triggerjenkins.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+#
+# Kind of backwards, but we'll trigger Jenkins from Ansible to start a new build
+# Also, when we get a production Jenkins, use a Jenkins user other than ksclarke
+#
+
+JOB_URL="https://jenkins.library.ucla.edu/job/sinai-web"
+JOB_STATUS_URL="${JOB_URL}/lastBuild/api/json"
+
+GREP_RETURN_CODE=0
+LOOP_LIMIT=24
+LOOP_COUNT=0
+
+# Start the build
+curl -u "ksclarke:${1}" -L -s "${JOB_URL}/buildWithParameters?delay=0sec&host=localhost&token=${2}"
+
+# Poll every ten seconds until the build is finished
+while [ $GREP_RETURN_CODE -eq 0 ]
+do
+    sleep 10
+    LOOP_COUNT=$(($LOOP_COUNT+1))
+
+    # Let's add a sanity check to avoid endless looping
+    if [ "$LOOP_COUNT" -ge "$LOOP_LIMIT" ]; then
+      break
+    fi
+
+    # Grep should return a 0 while the build is running
+    curl -u ksclarke:${1} -s ${JOB_STATUS_URL} | grep "result\":null" > /dev/null
+    GREP_RETURN_CODE=$?
+done
