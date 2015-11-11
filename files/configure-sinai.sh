48e900a91e2c671ec707dc7b70001426aaab2d6a
Kevin S. Clarke <ksclarke@gmail.com>
2015-11-11 17:58:45 -0500

    Fix transposed configuration values

--- files/configure-sinai.sh
+++ files/configure-sinai.sh
@@ -13,6 +13,6 @@ mv /etc/sinai/sample-config.json $CONFIG_FILE
 
 # Overwrite default values in the template file
 sed -i -e "s|587760109846\-8ctkp2qbuag2n7kh0lnd0vv8ur5u1os9\.apps\.googleusercontent\.com|$1|g" $CONFIG_FILE
-sed -i -e "s|LVNtnfPxmnTvBsKm9UumVmwiS|$2|g" $CONFIG_FILE
-sed -i -e "s|403322423208635|$3|g" $CONFIG_FILE
+sed -i -e "s|403322423208635|$2|g" $CONFIG_FILE
+sed -i -e "s|LVNtnfPxmnTvBsKm9UumVmwiS|$3|g" $CONFIG_FILE
 sed -i -e "s|jE8sqlZ9skUg1GtEZtu6bw1e0tmMteXHXNKbbQHAOb39yvMMAK|$4|g" $CONFIG_FILE
